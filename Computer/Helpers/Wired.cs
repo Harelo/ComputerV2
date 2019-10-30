98e5ba03d396bf4030ec24179a8e0c2559bb3396
Hareloo <harel123@windowslive.com>
2019-10-30 10:36:32 +0200

    Change: IWired to Wired

---
+++ Computer/Helpers/Wired.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Computer.Helpers
+{
+    public abstract class Wired
+    {
+        protected Wire input, output;
+
+        public Wired()
+        {
+            input = new Wire();
+            output = new Wire();
+        }
+    }
+}

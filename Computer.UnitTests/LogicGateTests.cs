8f98f8fb4932f08940ce18ae3e2fb6dca760971c
Hareloo <harel123@windowslive.com>
2019-10-30 13:22:10 +0200

    Add: AND gate

--- Computer.UnitTests/LogicGateTests.cs
+++ Computer.UnitTests/LogicGateTests.cs
@@ -19,5 +19,29 @@ namespace Computer.UnitTests
             // Assert
             Assert.IsFalse(notGate.Output.value);
         }
+
+        [TestMethod]
+        public void AND_InputsChanged_OutputUpdateCorrectly()
+        {
+            //When both inputs are off output should be off
+            AND andGate = new AND();
+            Assert.IsFalse(andGate.Output.value);
+
+            //When InputA only is on output should be off
+            andGate.InputA.value = true;
+            Assert.IsFalse(andGate.Output.value);
+
+            //When both inputs are on output should be on
+            andGate.InputB.value = true;
+            Assert.IsTrue(andGate.Output.value);
+
+            //When InputB only is on output should be off
+            andGate.InputA.value = false;
+            Assert.IsFalse(andGate.Output.value);
+
+            //When both inputs are off again output should remain off
+            andGate.InputB.value = false;
+            Assert.IsFalse(andGate.Output.value);
+        }
     }
 }
--- Computer/LogicGates/AND.cs
+++ Computer/LogicGates/AND.cs
@@ -3,9 +3,36 @@ using Computer.Interfaces;
 
 namespace Computer.LogicGates
 {
+    /// <summary>
+    /// A gate with two inputs and an output. Only when both inputs are on the output is on
+    /// </summary>
     public class AND : IWired
     {
         public Wire InputA { get; set; }
+        public Wire InputB { get; set; }
         public Wire Output { get; set; }
+
+        public AND()
+        {
+            InputA = new Wire();
+            InputB = new Wire();
+            Output = new Wire();
+
+            //Events for updating the output depending on the inputs
+
+            InputA.WireUpdateEvent += (n) =>
+            {
+                if (InputB.value && n)
+                    Output.value = true;
+                else Output.value = false;
+            };
+
+            InputB.WireUpdateEvent += (n) =>
+            {
+                if (InputA.value && n)
+                    Output.value = true;
+                else Output.value = false;
+            };
+        }
     }
 }

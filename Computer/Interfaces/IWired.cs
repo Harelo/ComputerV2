5b5dcf32bc3192cf55900ff38f687c722ece5a7b
Hareloo <harel123@windowslive.com>
2019-10-30 10:52:30 +0200

    Change: Wired back to IWired

---
+++ Computer/Interfaces/IWired.cs
@@ -0,0 +1,10 @@
+﻿using Computer.Helpers;
+
+namespace Computer.Interfaces
+{
+    public interface IWired
+    {
+        public Wire Input { get; set; }
+        public Wire Output { get; set; }
+    }
+}
